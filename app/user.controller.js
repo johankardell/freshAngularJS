1fcec9c8051b55443f4da531b387a4c36b5a8fde
Johan Kardell <johan.kardell@softhouse.se>
2016-02-23 11:00:25 +0100

    Routing demo

---
+++ app/user.controller.js
@@ -0,0 +1,11 @@
+/* global angular */
+
+(function (module) {
+    'use strict';
+    module.controller('userController', userController);
+    userController.$inject = ['$scope', '$routeParams'];
+
+    function userController($scope, $routeParams) {
+        $scope.user = $routeParams.username;
+    }
+} (angular.module('app.angularJS')));
