2e85e6aa803bb4edd088871e6bfbade9d992c1d4
Johan Kardell <johan.kardell@softhouse.se>
2016-02-23 11:06:09 +0100

    namefactory

---
+++ app/name.factory.js
@@ -0,0 +1,11 @@
+/* global angular */
+
+(function (module) {
+    'use strict';
+    module.factory('nameFactory', nameFactory);
+    nameFactory.$inject = [];
+
+    function nameFactory() {
+
+    }
+} (angular.module('app.angularJS')));
